b05e2b79b788ac469f416c6167c5da9a5618c4b3
Shravan6488 <32151014+Shravan6488@users.noreply.github.com>
2019-02-23 10:24:28 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,77 +1,9 @@
-variable "region" {
-  default = "europe-west1"
+#####################################################################
+# Variables
+#####################################################################
+variable "project" {}
+variable "region" {}
+variable "username" {
+  default = "admin"
 }
-
-variable "cluster_name" {
-  default = "tf-regional"
-}
-
-variable "master_version" {
-  default = ""
-}
-
-variable "node_count" {
-  default = 1
-}
-
-variable "tags" {
-  type    = "list"
-  default = []
-}
-
-variable "network" {
-  default = "default"
-}
-
-variable "subnetwork" {
-  default = "default"
-}
-
-data "google_compute_zones" "available" {
-  region = "${var.region}"
-}
-
-data "google_container_engine_versions" "default" {
-  zone = "${element(data.google_compute_zones.available.names, 0)}"
-}
-
-resource "google_container_cluster" "default" {
-  name               = "${var.cluster_name}"
-  region             = "${var.region}"
-  initial_node_count = "${var.node_count}"
-  min_master_version = "${var.master_version != "" ? var.master_version : data.google_container_engine_versions.default.latest_master_version}"
-  network            = "${var.network}"
-  subnetwork         = "${var.subnetwork}"
-
-  enable_legacy_abac = true
-
-  node_config {
-    tags = ["${var.tags}"]
-  }
-
-  // Wait for the GCE LB controller to cleanup the resources.
-  provisioner "local-exec" {
-    when    = "destroy"
-    command = "sleep 90"
-  }
-}
-
-output "instance_groups" {
-  value = "${google_container_cluster.default.instance_group_urls}"
-}
-
-output "endpoint" {
-  value = "${google_container_cluster.default.endpoint}"
-}
-
-output "client_certificate" {
-  value = "${google_container_cluster.default.master_auth.0.client_certificate}"
-}
-
-output "client_key" {
-  value = "${google_container_cluster.default.master_auth.0.client_key}"
-}
-
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.default.master_auth.0.cluster_ca_certificate}"
-}
+variable "password" {}
