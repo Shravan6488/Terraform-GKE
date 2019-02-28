b545deb5b13988535b578bc91e0e1bb6421c1950
Shravan6488 <32151014+Shravan6488@users.noreply.github.com>
2019-02-28 22:09:40 +0800

    Update services.tf

--- k8s/services.tf
+++ k8s/services.tf
@@ -1,3 +1,17 @@
+resource "kubernetes_pod" "echo" {
+  metadata {
+    name = "echo-example"
+    labels {
+      App = "echo"
+  } }
+  spec {
+    container {
+      image = "hashicorp/http-echo:0.2.1"
+      name  = "example2"
+      args = ["-listen=:80", "-text='Hello World'"]
+      port {
+        container_port = 80
+} } } }
 resource "kubernetes_replication_controller" "nginx" {
   metadata {
     name = "shravan-nginx-example"
@@ -55,4 +69,22 @@ resource "kubernetes_service" "nginx" {
 
     type = "LoadBalancer"
   }
+}
+resource "kubernetes_service" "echo" {
+  metadata {
+    name = "echo-example"
+  }
+  spec {
+    selector {
+      App = "${kubernetes_pod.echo.metadata.0.labels.App}"
+    }
+    port {
+      port        = 80
+      target_port = 80
+    }
+#    type = "LoadBalancer"
+} }
+
+output "lb_ip" {
+  value = "${kubernetes_service.echo.load_balancer_ingress.0.ip}"
 }
